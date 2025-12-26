a771441b8b19a6c65f7953735b4a56c06198e368
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2025-12-26 12:31:48 +0530

    map-style class implementation + dataloader

---
+++ Creating Input-Output Pairs/generate_dataset.py
@@ -0,0 +1,36 @@
+from torch.utils.data import Dataset, DataLoader
+import torch
+import tiktoken
+
+class GPTDataset(Dataset):
+    def __init__(self,txt,tokenizer,max_length,stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt,allowed_special={"<|endoftext|>"})
+
+        for i in range(0,len(token_ids)-max_length,stride):
+            input_chunk = token_ids[i:i+max_length]
+            target_chunk = token_ids[i+1:i+max_length+1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __get_item__(self,idx):
+        return self.input_ids[idx],self.target_ids[idx]
+    
+def create_dataloader(txt,batch_size=4,max_length=256,stride=128,shuffle=True,drop_last=True,num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDataset(txt,tokenizer,max_length,stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
